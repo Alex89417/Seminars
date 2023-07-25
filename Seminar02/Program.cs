c1fccbd3e388e3f936b158c9a67e573e5769d231
Alex89417 <zezylia25@gmail.com>
2023-07-25 22:31:12 +0300

    Fixed Seminar 2

--- Seminar02/Program.cs
+++ Seminar02/Program.cs
@@ -1,40 +1,39 @@
-﻿// Theory
-/* 
+﻿// Theory:
+/*  
 13254 / 10 = 1325
 13254 / 100 = 132
 13254 / 1000 = 13
 13254 / 10000 = 1
 
 13254 % 10 = 4
-13254 % 10 = 54
-13254 % 10 = 254
-13254 % 10 = 3254
+13254 % 100 = 54
+13254 % 1000 = 254
+13254 % 10000 = 3254
 
-24365 -> 43 = % 100 % 10
+24365 -> 43 = / 100 = 243 % 100 = 43
 
-*/
-
-/*
 Метод -вызываемоя подпрограмма
 */
 
+// Task:
 /* Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа:
+
+
 // int CutNumber(int num)
 // {
 //     int hundreds = num / 100;
 //     int units = num % 10;
 
-//     int res = hundreds * 10 + units;
-//     return res;
+//     return hundreds * 10 + units;
 // }
-
 
 int CutNumber(int num)
 {
     int hundreds = num / 100;
     int units = num % 10;
 
-    return hundreds * 10 + units;
+    int res = hundreds * 10 + units;
+    return res;
 }
 
 
@@ -46,7 +45,7 @@ Console.WriteLine($"New version of {randNumber} is {newNumber}");
 
 /* Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b:
 
-Bool IsDivisible(int num, int div, int div2)
+bool IsDivisible(int num, int div1, int div2)
 {
     // if(num % div1 == 0 && num % div2 == 0)
     //     return true;
@@ -59,9 +58,9 @@ Bool IsDivisible(int num, int div, int div2)
 Console.Write("Input a number for checking: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a first divider: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int divider1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second divider: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int divider2 = Convert.ToInt32(Console.ReadLine());
 
 bool res = IsDivisible(number, divider1, divider2);
 
@@ -80,16 +79,17 @@ int MaxNum(int num)
 }
 int number = new Random().Next(10, 100);  // [a;b]
 Console.WriteLine(number);
+
 int res = MaxNum(number);
 Console.WriteLine(res);
 */
 
-//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого:
+/* Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого:
 
 
 bool SquerCheck(int firstNum, int secondNum)
 {
-    return (firstNum == secondNum || secondNum == firstNum * firstNum);
+    return (firstNum == secondNum * secondNum || secondNum == firstNum * firstNum);
 }
 
 Console.Write("Input the first number: ");
@@ -100,7 +100,7 @@ int numberTwo = Convert.ToInt32(Console.ReadLine());
 bool res = SquerCheck(numberOne, numberTwo);
 if(res) Console.WriteLine("Yes");
 else Console.WriteLine("No");
-
+*/
 
 
 
