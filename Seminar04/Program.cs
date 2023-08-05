97941e8f8366bb0b663769201184a6d72a9bfa2f
Alex89417 <zezylia25@gmail.com>
2023-08-05 15:33:59 +0300

    complited seminar 04

--- Seminar04/Program.cs
+++ Seminar04/Program.cs
@@ -94,3 +94,30 @@
 
 
 
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0 ; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+        
+    Console.WriteLine();
+}
+
+ Console.Write("input a length of an array: ");
+ int length = Convert.ToInt32(Console.ReadLine());
+ Console.Write("input a min possible value: ");
+ int min = Convert.ToInt32(Console.ReadLine());
+ Console.Write("input a man possible value: ");
+ int max = Convert.ToInt32(Console.ReadLine());
+
+ int[] myArray = CreateRandomArray(length, min, max);
+ WriteArray(myArray);
